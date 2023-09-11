fd873cee5aedb22c119adffc68c194cbd606323b
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-09-11 16:20:00 +0200

    Created authentication service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 
-import { JwtAuth } from "../models/jwtAuth";
-import { Login } from "../models/login";
-import { Register } from "../models/register";
+import { JwtAuth } from "../../models/jwtAuth";
+import { Login } from "../../models/login";
+import { Register } from "../../models/register";
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +13,19 @@ import { Register } from "../models/register";
 export class AuthenticationService {
   registerUrl = "Auth/register";
   loginUrl = "Auth/login";
+  refreshTokenUrl = "Auth/refresh-token"
 
   constructor(private http: HttpClient) { }
 
-  public register(user: Register): Observable<JwtAuth> {
-    return this.http.post<JwtAuth>(`${environment.apiUrl}/${this.registerUrl}`, user);
+  public register(user: Register): Observable<any> {
+    return this.http.post<any>(`${environment.apiUrl}/${this.registerUrl}`, user);
   }
 
   public login(user: Login): Observable<JwtAuth> {
     return this.http.post<JwtAuth>(`${environment.apiUrl}/${this.loginUrl}`, user);
   }
+
+  // public refreshToken(refreshToken: string): Observable<JwtAuth> {
+  //   return this.http.post<JwtAuth>(`${environment.apiUrl}/${this.refreshTokenUrl}`, refreshToken)
+  // }
 }
