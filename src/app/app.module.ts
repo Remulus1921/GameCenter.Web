40d4dced913031dc10e023ed3f24ea3cc9be9413
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-09-12 16:06:27 +0200

    Added new material elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { LoadingBarHttpClientModule } from "@ngx-loading-bar/http-client";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AuthenticationInterceptor } from "./core/interceptors/interceptor";
 import { MainModule } from "./main/main.module";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [
@@ -15,6 +17,8 @@ import { MainModule } from "./main/main.module";
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
     MainModule,
     AppRoutingModule,
     HttpClientModule,
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,9 +2,11 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 
+import { MaterialModule } from "../material.module";
 import { LoginModule } from "./account/login/login.module";
 import { RegisterModule } from "./account/register/register.module";
 import { FeaturesRoutingModule } from "./features-routing.module";
+import { PlatformModule } from "./platform/platform.module";
 
 
 @NgModule({
@@ -14,8 +16,10 @@ import { FeaturesRoutingModule } from "./features-routing.module";
     CommonModule,
     FeaturesRoutingModule,
     ReactiveFormsModule,
+    MaterialModule,
     LoginModule,
-    RegisterModule
+    RegisterModule,
+    PlatformModule
   ]
 })
 export class FeaturesModule { }
