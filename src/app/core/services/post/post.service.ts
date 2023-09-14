13604a6b0022796454bb21c988e020957cd67362
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-09-14 11:32:38 +0200

    Created post service

---
+++ src/app/core/services/post/post.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { PostAddUpdateDto, PostDto, PostSmallDto } from "src/app/models/post/postDto";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  postUrl = "Posts";
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<PostSmallDto[]> {
+    return this.http.get<PostSmallDto[]>(`${environment.apiUrl}/$${this.postUrl}`);
+  }
+
+  getPost(id: string): Observable<PostDto> {
+    return this.http.get<PostDto>(`${environment.apiUrl}/${this.postUrl}/${id}`);
+  }
+
+  addPost(post: PostAddUpdateDto): Observable<any> {
+    return this.http.post(`${environment.apiUrl}/${this.postUrl}`, post);
+  }
+
+  updatePost(id: string, post: PostAddUpdateDto): Observable<any> {
+    return this.http.put(`${environment.apiUrl}/${this.postUrl}/${id}`, post);
+  }
+
+  deletePost(id: string): Observable<any> {
+    return this.http.delete(`${environment.apiUrl}/${this.postUrl}/${id}`);
+  }
+}
