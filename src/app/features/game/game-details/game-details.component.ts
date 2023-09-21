08007b5ae734705689c13e6a2f472dd2b6a2956f
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-09-21 18:37:31 +0200

    Implemented getting user email from jwt token

--- src/app/features/game/game-details/game-details.component.ts
+++ src/app/features/game/game-details/game-details.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import jwt_decode from "jwt-decode";
 import { CommentService } from "src/app/core/services/comment/comment.service";
 import { GameService } from "src/app/core/services/game/game.service";
 import { RateService } from "src/app/core/services/rate/rate.service";
 import { CommentSmallDto } from "src/app/models/comment/commentDtos";
 import { GameDto } from "src/app/models/game/gameDtos";
 import { RateDto, RateSmallDto } from "src/app/models/rate/rateDtos";
+
 
 @Component({
   selector: 'app-game-details',
@@ -14,27 +16,33 @@ import { RateDto, RateSmallDto } from "src/app/models/rate/rateDtos";
 })
 export class GameDetailsComponent implements OnInit{
   game: GameDto = {} as GameDto;
-  id!: string;
+  id: string = String(this.route.snapshot.paramMap.get('id'))
   avarageRate: RateDto = {} as RateDto;
   userRate: RateSmallDto = {} as RateSmallDto;
   newComment: CommentSmallDto = {} as CommentSmallDto;
   rate: RateSmallDto = {} as RateSmallDto;
+  token: string | null = null;
+  decodedToken: any;
+  email: string = '';
 
   constructor(
     private _gameService: GameService,
     private _rateService: RateService,
     private _commentService: CommentService,
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     this.getGameDetails();
     this.getAvarageRate();
     this.getUserRate();
+    this.getUserEmail();
   }
 
   getGameDetails(): void {
-    this.id = String(this.route.snapshot.paramMap.get('id'));
-    this._gameService.getGame(this.id).subscribe((game) => this.game = game);
+    this._gameService.getGame(this.id).subscribe((game) => {
+      this.game = game;
+    });
   }
 
   getAvarageRate(): void {
@@ -53,15 +61,10 @@ export class GameDetailsComponent implements OnInit{
     this._rateService.updateRate(this.id, this.rate).subscribe(() => this.getUserRate());
   }
 
-  addComment(): void {
-    this._commentService.addComment(this.id, this.newComment).subscribe(() => this.getGameDetails());
+  getUserEmail(): void {
+    this.token = localStorage.getItem("jwtToken");
+    this.decodedToken= jwt_decode(this.token as string);
+    this.email = this.decodedToken.email;
   }
 
-  editComment(comment: CommentSmallDto): void {
-    this._commentService.updateComment(comment.id!, comment).subscribe(() => this.getGameDetails());
-  }
-
-  deleteComment(id: string): void {
-    this._commentService.deleteComment(id).subscribe(() => this.getGameDetails());
-  }
 }
