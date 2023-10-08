8fae6f7d5836aee0f5107ee2743a65570d9836a6
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-10-08 13:19:08 +0200

    added additional headers and changed sending data to Form Data

--- src/app/core/services/game/game.service.ts
+++ src/app/core/services/game/game.service.ts
@@ -1,31 +1,58 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { GameAddUpdateDto, GameDto, GameSmallDto } from "src/app/models/game/gameDtos";
-import { environment } from "src/environments/environment";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GameDto, GameSmallDto } from 'src/app/models/game/gameDtos';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameService {
-  gameUrl: string = "Games";
+  gameUrl: string = 'Games';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getGames(): Observable<GameSmallDto[]> {
-    return this.http.get<GameSmallDto[]>(`${environment.apiUrl}/${this.gameUrl}`);
+    return this.http.get<GameSmallDto[]>(
+      `${environment.apiUrl}/${this.gameUrl}`
+    );
   }
 
   getGame(id: string): Observable<GameDto> {
-    return this.http.get<GameDto>(`${environment.apiUrl}/${this.gameUrl}/${id}`);
+    return this.http.get<GameDto>(
+      `${environment.apiUrl}/${this.gameUrl}/${id}`
+    );
   }
 
-  addGame(game: GameAddUpdateDto): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/${this.gameUrl}/addGame`, game, {responseType: 'text'});
+  addGame(formData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      Accept: 'text/plain',
+    });
+
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.post(
+      `${environment.apiUrl}/${this.gameUrl}/addGame`,
+      formData,
+      { headers, responseType: 'text' }
+    );
   }
 
-  updateGame(id: string, game: GameAddUpdateDto): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/${this.gameUrl}/${id}`, game, {responseType: 'text'});
+  updateGame(id: string, formData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      Accept: 'text/plain',
+    });
+
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.put(
+      `${environment.apiUrl}/${this.gameUrl}/${id}`,
+      formData,
+      {
+        headers,
+        responseType: 'text',
+      }
+    );
   }
 
   deleteGame(id: string): Observable<any> {
