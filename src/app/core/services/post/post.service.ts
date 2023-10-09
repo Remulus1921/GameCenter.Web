26f7876b94b9df1ff09a1672c66f1393e3c192ae
Remigiusz Bekierz <103840769+Remulus1921@users.noreply.github.com>
2023-10-09 10:36:04 +0200

    Post service upadted to accept multipart form data

--- src/app/core/services/post/post.service.ts
+++ src/app/core/services/post/post.service.ts
@@ -1,31 +1,57 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { PostAddUpdateDto, PostDto, PostSmallDto } from "src/app/models/post/postDtos";
-import { environment } from "src/environments/environment";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostDto, PostSmallDto } from 'src/app/models/post/postDtos';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostService {
-  postUrl = "Posts";
+  postUrl = 'Posts';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPosts(): Observable<PostSmallDto[]> {
-    return this.http.get<PostSmallDto[]>(`${environment.apiUrl}/${this.postUrl}`);
+    return this.http.get<PostSmallDto[]>(
+      `${environment.apiUrl}/${this.postUrl}`
+    );
   }
 
   getPost(id: string): Observable<PostDto> {
-    return this.http.get<PostDto>(`${environment.apiUrl}/${this.postUrl}/${id}`);
+    return this.http.get<PostDto>(
+      `${environment.apiUrl}/${this.postUrl}/${id}`
+    );
   }
 
-  addPost(post: PostAddUpdateDto): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/${this.postUrl}`, post, {responseType: 'text'});
+  addPost(formData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      Accept: 'text/plain',
+    });
+
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.post(`${environment.apiUrl}/${this.postUrl}`, formData, {
+      headers,
+      responseType: 'text',
+    });
   }
 
-  updatePost(id: string, post: PostAddUpdateDto): Observable<any> {
-    return this.http.put(`${environment.apiUrl}/${this.postUrl}/${id}`, post, {responseType: 'text'});
+  updatePost(id: string, formData: FormData): Observable<any> {
+    const headers = new HttpHeaders({
+      Accept: 'text/plain',
+    });
+
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.put(
+      `${environment.apiUrl}/${this.postUrl}/${id}`,
+      formData,
+      {
+        headers,
+        responseType: 'text',
+      }
+    );
   }
 
   deletePost(id: string): Observable<any> {
